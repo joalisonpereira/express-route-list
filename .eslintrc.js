50cd2d742d347faf94b31c84d248336a532b4f2e
joalisonpereira <joalison.pereira@hotmail.com>
2023-02-08 14:36:31 -0300

    build: configure project tools

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: ['standard-with-typescript', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  rules: {}
+};
