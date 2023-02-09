b4436a2181860633bd0528ccc25d2798187df78d
joalisonpereira <joalison.pereira@hotmail.com>
2023-02-09 04:09:46 -0300

    test: add coverage badges

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 import path from 'node:path';
 
 export default defineConfig({
+  test: {
+    coverage: {
+      reporter: ['json-summary', 'lcov', 'text']
+    }
+  },
   resolve: {
     alias: {
       src: path.resolve(__dirname, './src')
