5933fa8c3486067d66543cc4d54c77b5d32551ca
joalisonpereira <joalison.pereira@hotmail.com>
2023-02-09 01:15:44 -0300

    build: modify build files

---
+++ tsup.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src', '!src/**/*.spec.*', '!src/tests/**', '!src/@types/**.d.ts'],
+  splitting: false,
+  sourcemap: false,
+  clean: false
+});
