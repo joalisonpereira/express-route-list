d3facf0daa4010b0b80beaa4128e3861699d59d4
joalisonpereira <joalison.pereira@hotmail.com>
2023-02-09 21:05:24 -0300

    build: remove mock from build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src', '!src/**/*.spec.*', '!src/tests/**', '!src/@types/**.d.ts'],
+  entry: [
+    'src',
+    '!src/**/*.spec.*',
+    '!src/tests/**',
+    '!src/mocks/*.ts',
+    '!src/@types/**.d.ts'
+  ],
   splitting: false,
   sourcemap: false,
   clean: false,
